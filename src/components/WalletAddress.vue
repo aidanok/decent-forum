2525c0a0a3c6764d151404fe02c3a2f9e4f35078
aokisok <sevenclickok@gmail.com>
2019-09-30 22:38:48 +0100

    add arweave-id query to WalletAddress component

--- src/components/WalletAddress.vue
+++ src/components/WalletAddress.vue
@@ -1,21 +1,34 @@
 <template>
   <span>
-    <span>{{start}} </span><span>...</span><span> {{end}}</span>
+    <span v-if="!handle">{{start}}</span>
+    <span v-else>{{handle}}</span>
   </span>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { queryArweaveId } from 'decent-forum-api/query/arweave-id';
 export default Vue.extend({
   props: {
     address: {
       type: String,
-      required: true, 
+      required: false, // SHOULD ACTUALLY BE REQUIRED... 
     }
   },
+  
+  data: () => ({
+    handle: null as string | null
+  }),
+
+  created() {
+    queryArweaveId(this.address)
+      .then(x => this.handle = x.handle)
+      .catch(e => {});
+  },
+
   computed: {
     start: function(): string {
-      return this.address.substr(0, 4);
+      return this.address.substr(0, 7);
     },
     end: function(): string {
       return this.address.substr(this.address.length-4);
