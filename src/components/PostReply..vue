9a4d747e2d21ea9c9e9f99c3171b8142ed59ae5e
aokisok <sevenclickok@gmail.com>
2019-09-30 22:38:18 +0100

    auto focus textarea on PostReply

--- src/components/PostReply..vue
+++ src/components/PostReply..vue
@@ -1,9 +1,25 @@
 <template>
   <div class="post-reply">
-    <textarea></textarea>
+    <textarea ref="textEditor" @blur="editorBlurred" placeholder="..."></textarea>
     <a role="button">
-      Post
+      Post Reply
       <i class="ri-send-plane-2-line"></i>
     </a>
   </div> 
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  mounted() {
+    (this.$refs.textEditor as any).focus();
+  },
+
+  methods: {
+    editorBlurred(event: any) {
+      console.log(event);
+      //this.$emit('blur');
+    }
+  }
+})
+</script>
